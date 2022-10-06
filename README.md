3454161a565f86a45334bde1a82cf4d7eb773d1f
BenBktech <bbrucher@gmail.com>
2022-10-06 16:04:47 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Using key encryption to deploy smart contracts
+
+This is a much more secure way of deploying smart contract with an account.
+
+The private key is from Ganache, there is no need to try to steal something there ;)
---
+++ encryptKey.js
@@ -0,0 +1,19 @@
+const ethers = require("ethers");
+const fs = require("fs");
+require("dotenv").config();
+
+async function main() {
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD,
+        process.env.PRIVATE_KEY
+    );
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
