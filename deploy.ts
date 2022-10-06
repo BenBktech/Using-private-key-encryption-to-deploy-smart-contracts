5c9230f203566e2f5a7ddb5919b2930aaf6b2cf0
BenBktech <bbrucher@gmail.com>
2022-10-06 16:57:39 +0200

    Added Typescript

--- deploy.ts
+++ deploy.ts
@@ -1,15 +1,18 @@
-const ethers = require("ethers");
-const fs = require("fs");
-require("dotenv").config();
+// const ethers = require("ethers");
+// const fs = require("fs");
+// require("dotenv").config();
+import { ethers } from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config";
 
 async function main() {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!);
     // const wallet = new ethers.Wallet(
     //     process.env.PRIVATE_KEY,
     //     provider
     // );
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    const encryptedJson: string = fs.readFileSync("./.encryptedKey.json", "utf8");
+    let wallet = new (ethers.Wallet.fromEncryptedJsonSync as any)(encryptedJson, process.env.PRIVATE_KEY_PASSWORD!);
     wallet = await wallet.connect(provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
---
+++ encryptKey.ts
@@ -0,0 +1,23 @@
+// const ethers = require("ethers");
+// const fs = require("fs");
+// require("dotenv").config();
+
+import { ethers } from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config";
+
+async function main() {
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!);
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD!,
+        process.env.PRIVATE_KEY!
+    );
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
